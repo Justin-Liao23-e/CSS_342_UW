820ac7f631d38a7be2daa86121b4528c8f953d57
AI Essen Liao <justcs123only@gmail.com>
2024-10-17 18:12:04 -0700

    Update 8

--- Program 1/vending_bank.h
+++ Program 1/vending_bank.h
@@ -7,16 +7,22 @@ public:
     // Constructors
     VendingBank();
     VendingBank(int id);
+    VendingBank(int id, int pennies, int nickels, int dimes, int quarters);
 
     //getters-setters
     int id() const;
-    void setId(int id);
+    int coin() const;
+    bool setId(int id);
+    bool setQuarters(int n);
+    bool setDimes(int n);
+    bool setNickels(int n);
+    bool setPennies(int n);
 
     // Buy Function
     bool buy(double price, int quantity); //allow customer to buy snacks; return true and reduce balance if enough balance, else return false
-
+    
     // addBal Function
-    void addBal(double amount); //add amount to the balance
+    void addBal(int pennies, int nickels, int dimes, int quarters); //add coins to the vending machine
 
     // getBal Function
     double getBal() const; //return current balance
@@ -24,10 +30,18 @@ public:
     // refund Function
     void refund(); //refund the balance by canceling the transaction
 
+    // Overloaad Operators
+    bool operator==(const VendingBank &other) const;
+
 private:
     // id_ is a unique identifier for the VendingBank
     // much like a serial number
     int id_;
+    // Different types of coins
+    int pennies_;
+    int nickels_;
+    int dimes_;
+    int quarters_;
     double balance_; //current balance of the customer
     double revenue_; //revenue of the vending machine
 };
